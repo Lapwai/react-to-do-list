80adbc399c6298be6c948c67a39d13513a2e7025
unknown <lapwailau@gmail.com>
2020-01-28 12:53:56 +1300

    Updates

---
+++ precache-manifest.1bf9f3cb17987157097cc3f77ecb1793.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf59785ca924c3739be117784419a82",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "70370086ea2a1a246222",
+    "url": "/react-to-do-list/static/css/main.b79c7064.chunk.css"
+  },
+  {
+    "revision": "bbb5a38eefcbe83f12ad",
+    "url": "/react-to-do-list/static/js/2.44b538b4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-to-do-list/static/js/2.44b538b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70370086ea2a1a246222",
+    "url": "/react-to-do-list/static/js/main.867c0ddd.chunk.js"
+  },
+  {
+    "revision": "cdad410f17ca030dad7b",
+    "url": "/react-to-do-list/static/js/runtime-main.c21664bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-to-do-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
